354d4c30ee7d24f6ca3ad088c09ea55d2d7bb79f
Yong Chean You <youyongchean@gmail.com>
2024-12-17 02:49:50 +0800

    #main: add frontend login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'login-frontend';
+  constructor(private http: HttpClient) {}
+  username = '';
+  password = '';
+
+  async login() {
+    // login api
+    this.http
+      .post('https://localhost:7014/login', {
+        username: this.username,
+        password: this.password,
+      })
+      .subscribe(
+        (res: any) => {
+          alert(`Login Status: ${res.message}`);
+        },
+        (error) => {
+          console.log('error', error);
+        }
+      );
+  }
 }
